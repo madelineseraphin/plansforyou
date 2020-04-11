91bea672a92a2fc0778537f8363b915730f53034
Ian Anderson <anderson.ia@husky.neu.edu>
2020-04-11 14:55:19 -0400

    add plans and availability components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from "styled-components";
+import PlanCard from "./components/PlanCard"
 
-function App() {
+export const formatString = "dddd, MMMM Do YYYY @ h:mm:ss a"
+
+export const PageBody = styled.div`
+  width: 1100px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const App = () => {
+  // const [plans, setPlans] = useState({});
+  const plans = [
+    {plan_id: 1,
+    title: 'Larz Anderson Park',
+    description_text: 'come hang out w us',
+    plan_photo: '',
+    start_time: '2020-04-24 20:00:00',
+    end_time: '2020-04-24 22:00:00',
+    host_name: 'Ian Anderson',
+    comments: [
+      {
+        friend_name: 'Maddie',
+        comment_text: 'sounds fun'
+      },
+      {
+        friend_name: 'Billie',
+        comment_text: 'i guess ill come'
+      }
+    ],
+    rsvps: [
+      {
+        friend_name: "maddie",
+        response: "going"
+      }
+    ]
+  }
+  ]
+
+  const myAvailibilities = [
+    {
+      availability_id: 1,
+      start_date: '2020-04-24 18:00:00',
+      end_date: '2020-04-24 22:00:00'
+    }
+  ]
+
+  const Plans = plans.map((plan) => <PlanCard plan={plan} />);
   return (
-    <>
-      <p>Hello world</p>
-    </>
+    <PageBody>
+      <h1>All plans</h1>
+      {Plans}
+      <h1>My Availabilities</h1>
+    </PageBody>
   );
 }
 
---
+++ src/components/PlanCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import moment from "moment";
+import { formatString } from "../App"
+
+const PlanCardWrapper = styled.div`
+  padding: 10px;
+  border: solid 1px black;
+`;
+
+const PlanCard = props => {
+  const { plan } = props;
+  const { title, host_name, start_time, end_time, plan_id } = plan;
+  const formatted_start = moment(start_time).format(formatString);
+  const duration = moment.duration(moment(end_time) - moment(start_time)).humanize();
+  return (
+    <PlanCardWrapper>
+      <p>{title}</p>
+      <p>{host_name}</p>
+      <p>When: {formatted_start}</p>
+      <p>Lasts {duration}</p>
+    </PlanCardWrapper>
+  );
+};
+
+export default PlanCard;
