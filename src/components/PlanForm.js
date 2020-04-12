4e46cf741e57c21aecbd1d1a6979c3cc9902cd44
Ian Anderson <anderson.ia@husky.neu.edu>
2020-04-12 14:58:51 -0400

    edit plan form

--- src/components/PlanForm.js
+++ src/components/PlanForm.js
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import api from '../api';
 import moment from 'moment';
+import styled from 'styled-components';
+
+const PlanFormTitle = styled.h1`
+    text-transform: capitalize
+`;
 
 const PlanForm = props => {
     const { plan_id, mode, user_id } = props;
-    // connection.sql('SET @title = ?;').bind(plan.title).execute(),
-    //         connection.sql('SET @description_text = ?;').bind(plan.description_text).execute(),
-    //         connection.sql('SET @plan_photo = ?;').bind(plan.plan_photo).execute(),
-    //         connection.sql('SET @start_time = ?;').bind(plan.start_time).execute(),
-    //         connection.sql('SET @end_time = ?;').bind(plan.end_time).execute(),
-    //         connection.sql('SET @host_id = ?;').bind(plan.host_id).execute(),
+
     const [plan, setPlan] = useState({ title: '', description_text: '', plan_photo: '', start_time: '', end_time: '', host_id: user_id });
+
+    useEffect(() => {
+        if (mode === 'edit') {
+            api.plans().getOne(plan_id).then((res) => {
+                const planInfo = res.data.plan[0] || {};
+                setPlan({
+                    title: planInfo.title,
+                    description_text: planInfo.description_text,
+                    plan_photo: planInfo.plan_photo,
+                    end_time: planInfo.end_time,
+                    host_id: planInfo.host_id
+                });
+            })
+        }
+      }, [mode, plan_id]);
+    
     const updateTitle = (val) => {
         setPlan({ ...plan, title: val });
     }
@@ -21,10 +37,10 @@ const PlanForm = props => {
         setPlan({ ...plan, plan_photo: val });
     }
     const updateStartTime = (val) => {
-        setPlan({ ...plan, start_time: moment(val).format('YYYY-MM-DD HH:mm:ss.SSSSS') });
+        setPlan({ ...plan, start_time: moment(val).format('YYYY-MM-DD HH:mm:ss') });
     }
     const updateEndTime = (val) => {
-        setPlan({ ...plan, end_time: moment(val).format('YYYY-MM-DD HH:mm:ss.SSSSS') });
+        setPlan({ ...plan, end_time: moment(val).format('YYYY-MM-DD HH:mm:ss') });
     }
     const createPlan = () => {
         if (plan.title && plan.description_text && plan.plan_photo && plan.start_time && plan.end_time) {
@@ -32,9 +48,15 @@ const PlanForm = props => {
             api.plans().create(plan).then((res) => console.log(res));
         }
     }
+    const editPlan = () => {
+        if (plan.title && plan.description_text && plan.plan_photo && plan.start_time && plan.end_time) {
+            console.log(plan)
+            api.plans().update(plan).then((res) => console.log(res));
+        }
+    }
     return (
         <>
-            <h1>Create a plan</h1>
+            <PlanFormTitle>{mode} a plan</PlanFormTitle>
             <div>
                 <span>Plan title:</span><input type="text" onChange={(e) => updateTitle(e.target.value)} />
             </div>
@@ -50,7 +72,7 @@ const PlanForm = props => {
             <div>
                 <span>End time:</span><input type="datetime-local" onChange={(e) => updateEndTime(e.target.value)} />
             </div>
-            <button onClick={() => createPlan()}>Create plan</button>
+            <button onClick={() => mode === 'edit' ? editPlan() : createPlan()}>{mode} plan</button>
         </>
     );
 };
